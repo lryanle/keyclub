b4695dc6461cb6424a4f31af1606bff5db7e968f
Ryan Le <lryanle@users.noreply.github.com>
2021-03-01 23:11:55 -0600

    Preview

--- js/index.js
+++ js/index.js
@@ -457,7 +457,7 @@ if (path.includes("calendar")) {
     e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
   }
 
-  if (path.includes("")) {
+  if (path.includes("") || path.includes("index")) {
     var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
     i.src = "https://cdn.curator.io/published/3e8bd139-4c09-4238-8cdf-07d62e6cabff_oglmd8dw.js";
     e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
